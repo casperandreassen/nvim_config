5bf3a34e37bfa6fc0a18cb9067d452d6d1751bd7
casper <c@andreassen.dev>
2024-09-28 07:38:47 +0200

    Add telescope.nvim and basic config.

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
+-- Set leader to be space
+vim.g.mapleader = " "
+
+
 require("config.lazy")
 
 
 -- Set catpuccin as the default color scheme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+-- Setup telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
