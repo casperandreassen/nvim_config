6ab8e493bec5d23fe909e25265967aeb4ad5ef04
casper <c@andreassen.dev>
2024-09-28 20:26:57 +0200

    Add LSP config.

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 -- Set leader to be space
 vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
 
 require("config.lazy")
 
