0134b045372dc04756add7bf54b168e097be2e96
casper <c@andreassen.dev>
2024-09-28 08:20:40 +0200

    Add gitignore and move config to plugin files.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,14 @@ local M = {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function()
+	local config = require("nvim-treesitter.configs")
+	config.setup({
+		ensure_installed = {"lua", "javascript", "python"},
+		highlight = { enable = true },
+		indent = { enable = true },
+	})
+    end
 }
 
 return { M }
